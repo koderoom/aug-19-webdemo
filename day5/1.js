0add4d78ef2a1af207d27c9fa2372a4122fe0371
santosh <santosh.ece06@gmail.com>
2019-10-02 11:40:40 +0530

    day5

--- day5/1.js
+++ day5/1.js
@@ -16,6 +16,32 @@ function addNewElement() {
     container.appendChild(newElementOrange);
 }
 
+
+function addNewElementUsingJQuery() {
+    let container = $('.container');
+
+    // first child :: blue box
+    let refElement = container.children().first();
+
+    // second child :: organge box
+    let refElement1 = container.children().first().next();
+
+    // clone of reference element
+    let newElementBlue = refElement.clone(true);
+    let newElementOrange = refElement1.clone(true);
+
+    container.first().append(newElementBlue);
+    container.first().append(newElementOrange);
+}
+
+function addNewElementUsingJQueryVer2() {
+    let blueBox = $('.container').children().first().clone(true);
+    let orangeBox = $('.container').children().first().next().clone(true);
+
+    $('.container').first().append(blueBox);
+    $('.container').first().append(orangeBox);
+}
+
 // TODO :: deleteing the first row child
 function deleteElement() {
     let container = document.querySelector('div');
